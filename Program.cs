99a8e38d542016b16a468d588a8446ad18a3e919
ElishevaLang <elp06990@gmail.com>
2025-01-15 18:05:17 +0200

    organize-code

--- Program.cs
+++ Program.cs
@@ -3,22 +3,15 @@ using Microsoft.EntityFrameworkCore;
 using TodoApi;
 
 var builder = WebApplication.CreateBuilder(args);
-
-
-
 // Configure DbContext with MySQL connection
 builder.Services.AddDbContext<ToDoDbContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("ToDoDB"), 
         Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.40-mysql")));
 
- /////////////
 builder.Services.AddControllers();
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-//////////////////////////////////////
-///
 // Configure CORS
 builder.Services.AddCors(options =>
 {
@@ -29,8 +22,6 @@ builder.Services.AddCors(options =>
                .AllowAnyMethod();
     });
 });
-//////////////////////////////////////
-
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
@@ -44,12 +35,9 @@ app.UseCors("AllowAll");
 
 app.MapGet("/", () => "Hello World!");
 
-
-// Get all items
 app.MapGet("/items", async (ToDoDbContext db) =>
     await db.Items.ToListAsync());
 
-// Add a new item
 app.MapPost("/items", async (ToDoDbContext db, Item newItem) =>
 {
     db.Items.Add(newItem);
@@ -63,9 +51,7 @@ app.MapPut("/items/{id}", async (ToDoDbContext db, int id, Item updatedItem) =>
     var item = await db.Items.FindAsync(id);
     if (item == null)
         return Results.NotFound();
-
     item.IsComplete = updatedItem.IsComplete;
-
     await db.SaveChangesAsync();
     return Results.NoContent();
 });
@@ -76,7 +62,6 @@ app.MapDelete("/items/{id}", async (ToDoDbContext db, int id) =>
     var item = await db.Items.FindAsync(id);
     if (item == null)
         return Results.NotFound();
-
     db.Items.Remove(item);
     await db.SaveChangesAsync();
     return Results.Ok(item);
